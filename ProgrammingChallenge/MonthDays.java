08cf4cf4e23bd596dfef44509b97c3c8024ee58a
guevarajf <guevarajf@gmail.com>
2025-08-08 09:26:29 +0800

    Challenge 2025-08-08

---
+++ ProgrammingChallenge/MonthDays.java
@@ -0,0 +1,76 @@
+/*
+Stored in:
+Repository:  https://github.com/guevarajf/java_nciii_2025batch3.git
+Folder: ProgrammingChallenge 
+Filename: MonthDays.java
+*/
+
+import java.util.Scanner;
+
+public class MonthDays {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int month;
+        
+        System.out.println("Month Days Calculator");
+        System.out.println("Enter 0 to quit");
+        System.out.println();
+        
+        do {
+            System.out.print("Enter a month number (1-12) or 0 to quit: ");
+            month = input.nextInt();
+            
+            switch (month) {
+                case 0:
+                    System.out.println("Thank you for using Month Days Calculator!");
+                    break;
+                case 1:
+                    System.out.println("January has 31 days.");
+                    break;
+                case 2:
+                    System.out.println("February has 28 days.");
+                    break;
+                case 3:
+                    System.out.println("March has 31 days.");
+                    break;
+                case 4:
+                    System.out.println("April has 30 days.");
+                    break;
+                case 5:
+                    System.out.println("May has 31 days.");
+                    break;
+                case 6:
+                    System.out.println("June has 30 days.");
+                    break;
+                case 7:
+                    System.out.println("July has 31 days.");
+                    break;
+                case 8:
+                    System.out.println("August has 31 days.");
+                    break;
+                case 9:
+                    System.out.println("September has 30 days.");
+                    break;
+                case 10:
+                    System.out.println("October has 31 days.");
+                    break;
+                case 11:
+                    System.out.println("November has 30 days.");
+                    break;
+                case 12:
+                    System.out.println("December has 31 days.");
+                    break;
+                default:
+                    System.out.println("Invalid month number! Please enter a number between 1 and 12.");
+                    break;
+            }
+            
+            if (month != 0) {
+                System.out.println();
+            }
+            
+        } while (month != 0);
+        
+        input.close();
+    }
+}
